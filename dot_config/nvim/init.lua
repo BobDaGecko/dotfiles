ebca792a0e126465054c8bcd442c1192b689601f
bobdagecko <dev@bobdagecko.dev>
2026-05-29 00:04:06 -0500

    feat: refactor critical startup configs to root initalization

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,1 +1,9 @@
+-- Enable faster startup by caching compiled Lua modules
+vim.loader.enable()
+
+-- Set <space> as the leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Initalize user package
 require("bobdagecko")
