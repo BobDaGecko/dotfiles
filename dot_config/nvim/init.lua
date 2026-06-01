4949d676e825a7915655ee06ac0ea5896c3255ac
BobDaGecko <dev@bobdagecko.dev>
2026-06-01 17:49:42 -0500

    feat: remove old nvim config in favor of LazyVim, to be added later

--- dot_config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
--- Enable faster startup by caching compiled Lua modules
-vim.loader.enable()
-
--- Set <space> as the leader key
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- Initalize user package
-require("bobdagecko")
--- dot_config/nvim/lua/bobdagecko/init.lua
+++
@@ -1,2 +0,0 @@
-require("bobdagecko.remap")
-require("bobdagecko.set")
